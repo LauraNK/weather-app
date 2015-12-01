22ea6a411ae503504b7559753a61df970c3c6afe
Laura <laura.kyle91@gmail.com>
2015-11-30 22:06:44 -0500

    Rewrote into an Angular app

--- js/weatherApp.js
+++ js/weatherApp.js
@@ -1,19 +1,70 @@
-$(document).ready(function() {
+var app = angular.module('weatherApp', []);
+app.controller('weatherAppCtrl', function($scope, $http) {
 
-  //Global scopes to store variables
-  var myScope = {};
-  var myUrl = {};
+  $scope.currentWeather = {};
+  $scope.allForecast = [];
+  $scope.userLocation = {};
 
-  function getDate() {
-      var monthNames = ["Jan.", "Feb.", "March", "April", "May", "June", "July", "Aug.", "Sept.", "Oct.", "Nov.", "Dec."];
-      var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-      var newDate = new Date();
-      var day = newDate.getDay();
-      var month = newDate.getMonth();
-      var date = newDate.getDate();
-      $("#date").html(dayNames[day] + " " + monthNames[month] + " " + date);
-      myScope.thisDay = dayNames[day];
-    } //End getDate
+  var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  //Detect location of user
+  getLocation = function() {
+    $.get("http://ip-api.com/json", function(data) {
+      $scope.userLocation.city = data.city;
+      $scope.userLocation.region = data.region;
+      $scope.userLocation.countryCode = data.countryCode.toLowerCase();
+      $scope.$apply();
+      getWeather();
+    }, 'jsonp');
+  };
+
+  getWeather = function() {
+    var apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + $scope.userLocation.city + ',' + $scope.userLocation.countryCode + '&APPID=a5bb3d3396a371e54916503720fca1a3';
+    console.log(apiUrl);
+
+    // Get current weather
+    $.get(apiUrl, function(data) {
+      $scope.currentWeather.currentTemp = toCel(data.main.temp);
+      $scope.currentWeather.maxTemp = toCel(data.main.temp_max);
+      $scope.currentWeather.weather = data.weather[0].description;
+      $scope.currentWeather.icon = data.weather[0].icon;
+      $scope.$apply();
+    });
+    getForecast();
+  };
+
+  getForecast = function() {
+    var forecastUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=' + $scope.userLocation.city + ',' + $scope.userLocation.countryCode + '&APPID=a5bb3d3396a371e54916503720fca1a3';
+    console.log(forecastUrl);
+
+    // Get three day forecast
+    $.get(forecastUrl, function(data) {
+
+      for (var i = 0; i < 3; i++) {
+        
+        // Temporary object
+        var temp = {};
+
+        var num = i * 8;
+        temp.minTemp = toCel(data.list[num].main.temp_min);
+        temp.maxTemp = toCel(data.list[num].main.temp_max);
+        temp.weather = data.list[num].weather[0].main;
+        temp.icon = data.list[num].weather[0].icon;
+
+        $scope.allForecast.push(temp);
+        $scope.$apply();
+        console.log($scope.allForecast);
+        console.log(num);
+      };
+    });
+
+  };
+  
+  //Convert to celsius
+  function toCel(temperature) {
+    temperature = (Math.floor(temperature - 273.15));
+    return temperature;
+  };
 
   //Set time
   setInterval(function() {
@@ -29,95 +80,15 @@ $(document).ready(function() {
 
   }, 1000);
 
-  getInfo = function() {
-    $.get("http://ip-api.com/json", function(result) {
-      myScope.city = result.city;
-      myScope.region = result.region;
-      myScope.countryCode = result.countryCode.toLowerCase();
-      $('#location').html(myScope.city);
-      myUrl.apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + myScope.city + "," + myScope.countryCode + "&APPID=a5bb3d3396a371e54916503720fca1a3";
-      console.log(myUrl);
-      getWeather();
-      getForecast();
-      console.log(myUrl, myScope)
-    }, "jsonp");
-
-  };
-
-  getWeather = function() {
-
-    $.get(myUrl.apiUrl, function(data) {
-      function setCel() {
-          $('#currentTemp').html(Math.floor((data.main.temp - 273.15)) + '&#176C');
-          $('#maxTemp').html(Math.floor((data.main.temp_max - 273.15)) + '&#176C');
-        }
-        //Convert to Fahr 
-      $('#fahr').click(function() {
-        $('#currentTemp').html(Math.floor((data.main.temp - 273.15) * 1.8000 + 32.00) + '&#176F');
-        $('#maxTemp').html(Math.floor((data.main.temp_max - 273.15) * 1.8000 + 32.00) + '&#176F');
-      });
-
-      $('#cel').click(function() {
-        setCel();
-      });
-      setCel();
-      $('#weather').html(data.weather[0].description);
-      $('#icon').append("<img src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png'>");
-
-    }, "jsonp");
-  };
-
-  getForecast = function() {
-    var forecastUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + myScope.city + "," + myScope.countryCode + "&APPID=a5bb3d3396a371e54916503720fca1a3";
-    console.log(forecastUrl);
-    $.get(forecastUrl, function(data) {
-
-      var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
+  function getDate() {
+      var monthNames = ["Jan.", "Feb.", "March", "April", "May", "June", "July", "Aug.", "Sept.", "Oct.", "Nov.", "Dec."];
       var newDate = new Date();
+      var day = newDate.getDay();
+      var month = newDate.getMonth();
+      var date = newDate.getDate();
+      $("#date").html(dayNames[day] + " " + monthNames[month] + " " + date);
+    };
 
-      var tomorrow = newDate.getDay() + 1;
-      var secondDay = newDate.getDay() + 2;
-      var thirdDay = newDate.getDay() + 3;
-      //If end of week, reset
-      if (tomorrow === 7) {
-        tomorrow = 0;
-      }
-      if (secondDay === 7) {
-        secondDay = 0;
-      } else if (secondDay === 8) {
-        secondDay = 1;
-      }
-      if (thirdDay === 7) {
-        thirdDay = 0;
-      } else if (thirdDay === 8) {
-        thirdDay = 1;
-      } else if (thirdDay === 9) {
-        thirdDay = 2;
-      }
-
-      $('#tomorrow h3').html(dayNames[tomorrow]);
-      $('#tomorrow .icon').append("<img src='http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png'>");
-      $('#tomorrow .weatherDisplay').html(data.list[0].weather[0].main);
-      $('.tempDisplay2 .min1').html(Math.floor(data.list[0].main.temp_min - 273.15) + '&#176C');
-      $('.tempDisplay2 .max1').html(Math.floor(data.list[0].main.temp_max - 273.15) + '&#176C');
-
-      $('#secondDay h3').html(dayNames[secondDay]);
-      $('#secondDay .icon').append("<img src='http://openweathermap.org/img/w/" + data.list[8].weather[0].icon + ".png'>");
-      $('#secondDay .weatherDisplay').html(data.list[8].weather[0].main);
-      $('.tempDisplay2 .min2').html(Math.floor(data.list[8].main.temp_min - 273.15) + '&#176C');
-      $('.tempDisplay2 .max2').html(Math.floor(data.list[8].main.temp_max - 273.15) + '&#176C');
-
-      $('#thirdDay .weatherDisplay').html(data.list[16].weather[0].main);
-
-      $('#thirdDay h3').html(dayNames[thirdDay]);
-      $('#thirdDay .icon').append("<img src='http://openweathermap.org/img/w/" + data.list[16].weather[0].icon + ".png'>");
-      $('.tempDisplay2 .min3').html(Math.floor(data.list[16].main.temp_min - 273.15) + '&#176C');
-      $('.tempDisplay2 .max3').html(Math.floor(data.list[16].main.temp_max - 273.15) + '&#176C');
-
-    }, "jsonp");
-  };
-
+  getLocation();
   getDate();
-  getInfo();
 });
